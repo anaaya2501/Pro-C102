ca22ab63d598e169e63a1f0c9ac1e3e047ea6ea3
anaaya2501 <71642248+anaaya2501@users.noreply.github.com>
2021-11-16 18:08:10 +0530

    Add files via upload

---
+++ Pro102.py
@@ -0,0 +1,5 @@
+import os
+originalfilename=input("Enter File Path with File Name")
+newfilename=input("Enter the File Path with New File Name")
+os.rename(originalfilename,newfilename)
+print("file renamed succesfully")
